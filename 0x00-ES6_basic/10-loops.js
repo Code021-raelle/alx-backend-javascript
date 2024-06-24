20fc0b75febd1d196fb0143a587c78510d4d6bd1
Code021-raelle <gabrielakinshola3@gmail.com>
2024-06-24 17:03:18 +0100

    backend javascript

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
-    const result = [];
-    for (let value of array) {
-        result.push(appendString + value);
-    }
-    return result;
+  const result = [];
+  for (const value of array) {
+    result.push(appendString + value);
+  }
+  return result;
 }
--- 0x00-ES6_basic/8-getBudgetCurrentYear.js
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -1,16 +1,16 @@
 function getCurrentYear() {
-    const date = new Date();
-    return date.getFullYear();
+  const date = new Date();
+  return date.getFullYear();
 }
 
 export default function getBudgetForCurrentYear(income, gdp, capita) {
-    const currentYear = getCurrentYear();
+  const currentYear = getCurrentYear();
 
-    const budget = {
-        [`income-${currentYear}`]: income,
-        [`gdp-${currentYear}`]: gdp,
-        [`capita-${currentYear}`]: capita,
-    };
+  const budget = {
+    [`income-${currentYear}`]: income,
+    [`gdp-${currentYear}`]: gdp,
+    [`capita-${currentYear}`]: capita
+  };
 
-    return budget;
+  return budget;
 }
